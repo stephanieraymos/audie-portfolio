e69c3a5931cd67554acd19f178a628540a8ba308
stephanieraymos <stephanieraymos@gmail.com>
2022-08-06 23:31:01 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import AboutMe from './components/AboutMe.vue'
+</script>
+
+<template>
+  <about-me />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AboutMe.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about-container">
+        <h1>Audie Raymos</h1>
+        <p>My name is Audie</p>
+        <p>Here is some about me text.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.about-container {
+   margin: 0 auto;
+}
+</style>
