a509eed7c89877aee9fbc238abed3a94a64da050
stephanieraymos <stephanieraymos@gmail.com>
2022-08-22 21:53:44 -0700

    Working! Webpack can now see the images without workaround

--- src/components/tabs/LogoTab.vue
+++ src/components/tabs/LogoTab.vue
@@ -1,9 +1,29 @@
 <template>
   <div>
-    <h1>LOGOGS</h1>
+    <h1>LOGOS</h1>
+    <div class="container">
+      <div v-for="logo in logos" :key="logo.title" class="logos">
+        <div class="title">{{ logo.title }}</div>
+        <!-- <img src="../../assets/headshot.jpg" alt="logo.src" /> -->
+        <!-- <img :src="getImgUrl(logo.src)" :alt="logo.src" /> -->
+        <img :src="`images/${logo.src}.jpg`" :alt="logo.src" />
+        <!-- portfolio/src/assets/headshot.jpg -->
+        <!-- <img :src="'../../assets/' + logo.src" :alt="logo.src"> -->
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const logos = [
+  { title: "Some Title", src: "headshot" },
+  { title: "Another Title", src: "headshot" },
+];
+
+// function getImgUrl(pic) {
+//   var images = require.context("../assets/", false, /\.png$/);
+//   return images("./" + pic + ".jpg");
+// }
+</script>
 
 <style lang="scss" scoped></style>
